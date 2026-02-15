4aac01fb0916ea8ece94624ee1639b1fbdfc7728
Deepsayan Das <deepsayandas274@gmail.com>
2026-02-15 15:32:53 +0530

    added parsing config file

---
+++ internal/config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Addr string
+}
+
+//env-default:"production"
+
+type Config struct {
+	Env         string `yaml :"env" env:"ENV env-required:"true" `
+	storagePath string `yaml : "storage_path" env-required: "true"`
+	HTTPServer  `yaml: "http_server"`
+}
+
+func handleError(err error) {
+
+}
+
+func MustLoad() *Config { //CONVENTION:starts with must means this program should run properly inorder to start the application properly if this throws and error DONOT start the application
+	//were not returning error as it is a must function so donot return the error just close the execution
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the config file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("Config Path not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config File Doesn't exist: %s", configPath)
+	}
+
+	var cfg Config
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		log.Fatalf("cannot read config file %s", err.Error())
+	}
+
+	return &cfg
+}
